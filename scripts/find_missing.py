d4397f2aa80c440c19be055b79e8a268ac74c128
Sean Pesce <pescesean@gmail.com>
2023-12-20 07:26:44 -0500

    Update missing network checker script

--- scripts/find_missing.py
+++ scripts/find_missing.py
@@ -18,11 +18,12 @@ Evidence for missing Ghost Networks:
 """
 
 import os
+import sys
 from datetime import datetime, timedelta
 
 
 MISSING_NETWORKS = []
-DIR = os.path.join('..', 'ghost_networks')
+DIR = ''
 FNAME_PREFIX = ''
 # Release date of Ghost Networks feature:
 # https://www.rockpapershotgun.com/deus-ex-breach-launches-random-daily-networks
@@ -33,6 +34,13 @@ if __name__ == '__main__':
     now = datetime.now()
     END_DATE = datetime(now.year, now.month, now.day)  # Today
     END_DATE += timedelta(days=1)  # Tomorrow
+    
+    if '-h' in sys.argv or '--help' in sys.argv:
+        print(f'Usage:\n\n{sys.argv[0]} [Ghost Networks directory]\n\nIf no folder path is provided, the script checks the current working directory for Ghost Network configurations.\n')
+        sys.exit()
+    
+    if len(sys.argv) > 1:
+        DIR = sys.argv[1]
 
     cur_day = START_DATE
     while cur_day != END_DATE:
